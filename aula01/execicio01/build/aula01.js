ebd50badf18716465a342c25ee80c294ab730f2b
jkmacielbr <jk.maciel.br@gmail.com>
2023-05-29 20:28:27 -0400

    criando um array de livros

--- aula01/execicio01/build/aula01.js
+++ aula01/execicio01/build/aula01.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Hello World!");
+const livros1 = [
+    {
+        nome: "A Menina que rouba livros",
+        preco: 90.00,
+        categoria: "Guerra",
+    },
+    {
+        nome: "O vendedor de Sonhos",
+        preco: 23.00,
+        categoria: "ajuda"
+    },
+    {
+        nome: "Eu sou o numero 7",
+        preco: 78.00,
+        categoria: "Heroi"
+    }
+];
+livros1.map((livro) => {
+    console.log("Nome do Livro: ", livro.nome);
+    console.log("Preco do Livro: ", livro.preco);
+    console.log("Categoria do Livro: ", livro.categoria);
+});
--- aula01/execicio01/src/aula01.ts
+++ aula01/execicio01/src/aula01.ts
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+interface Livros {
+    nome: string;
+    preco: number;
+    categoria: string;
+}
+
+const livros1: Livros[] = [
+    {
+        nome: "A Menina que rouba livros",
+        preco: 90.00,
+        categoria: "Guerra",
+    },
+    {
+        nome: "O vendedor de Sonhos",
+        preco: 23.00,
+        categoria: "ajuda"
+
+    },
+
+    {
+        nome: "Eu sou o numero 7",
+        preco: 78.00,
+        categoria: "Heroi"
+    }
+
+]
+
+livros1.map((livro) => {
+    console.log("Nome do Livro: ", livro.nome);
+    console.log("Preco do Livro: ", livro.preco);
+    console.log("Categoria do Livro: ", livro.categoria);
+})
+    
